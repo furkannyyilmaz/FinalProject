7825960583cf8946ae1e59022d955ea11e1522ed
Furkan <furkanyilmaz-@hotmail.com>
2021-02-24 18:16:26 +0300

    9.Hafta sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,7 @@
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -21,6 +22,21 @@ namespace Business.Concrete
         {
             // iş kodları
             return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> getByUnitPrice(decimal min, decimal max)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            return _productDal.GetProductDetails();
         }
     }
 }
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,12 +11,25 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {                               //: EfEntityRepositoryBase<Product,NorthwindContext implemente ederek crud işlemlerini yazmamıza gerek kalmadı.
-    public class EfProductDal :EfEntityRepositoryBase<Product,NorthwindContext>, IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
         //bütün veri tabanı operasyonları yazılmaya hazır..
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            using (NorthwindContext context= new NorthwindContext())
+            {
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto 
+                             {
+                                 ProductId = p.ProductId, ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName, UnitsInStock = p.UnitsInStock 
+                             };
 
-
-
+                return result.ToList();
+            }
+        }
     }
 }
 
