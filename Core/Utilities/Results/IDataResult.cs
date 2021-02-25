f49b3eb27d9ae73dea5114d5d45e83d0041ab4dd
Furkan <furkanyilmaz-@hotmail.com>
2021-02-25 18:30:15 +0300

    10.Hafta sonu

---
+++ Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{                                // hangi tipi döndüreceğini bana söyle
+    public interface IDataResult<T>:IResult
+    {
+        T Data { get; }
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //private bool v1;
+        //private string v2;
+                                                    // sen çalış ama alttaki constraucture ı da çalıştır.
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+           //Success = success;
+
+        }
+
+        public Result(bool success)
+        {
+            //overload
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get;}
+    }
+}
